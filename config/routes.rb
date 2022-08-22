a3631c2ada04b867e0ca94e5bb943e29ab7f8f11
Lolita Lugand <lyanlola77@gmail.com>
2022-08-22 17:31:04 +0200

    routing ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,21 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:new, :create] do
+    resources :dragons, only: [:index, :show]
+    resources :bookings, except: [:destroy]
+  end
+
+  resources :dragons, only: [:index, :show] do
+    resources :bookings, except: [:destroy]
+  end
+
+  resources :bookings, except: [:destroy] do
+    resources :reviews, only: [:index, :show, :new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
