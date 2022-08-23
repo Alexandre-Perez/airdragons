f7015c38a96bd93a602946f03dce91480de2e06f
Lolita Lugand <lyanlola77@gmail.com>
2022-08-23 11:07:53 +0200

    review controller et view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,37 @@
 class ReviewsController < ApplicationController
-  def  index
-  end
+  before_action :set_review, only: %i[new create destroy]
+  # def index
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.dragon = @dragon
+    if @review.save
+      redirect_to dragon_path(@dragon)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to dragon_path(@review.dragon), status: :see_other
+  end
+
+  private
+
+  def set_review
+    @dragon = Dragon.find(params[:dragon_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment)
   end
 end
